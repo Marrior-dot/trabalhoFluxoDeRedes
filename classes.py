82ab5e004b2d8d57c3ccb93860e62ccd2caf1dad
Marrior-dot <mrlimaroberto@gmail.com>
2023-06-25 18:09:33 -0300

    consertando o método de adjacências

--- classes.py
+++ classes.py
@@ -6,7 +6,7 @@ class Vertice:
         return self.content
     
 class Aresta:
-    def __init__(self,ce,fe,u,v):
+    def __init__(self,ce,fe,u: Vertice,v: Vertice):
         self.fe = fe
         self.ce = ce 
         self.u = u
@@ -52,6 +52,9 @@ class Rede:
     def get_arestas(self):
         return self.arestas
     
+    def set_fluxo(self, novo_fluxo):
+        self.F = novo_fluxo
+    
     def fe0(self):
         for aresta in self.arestas:
             aresta.set_fluxo(0)
@@ -59,21 +62,38 @@ class Rede:
     def adj(self, vertice: Vertice):
         adjs = []
         for aresta in self.arestas:
+            
+            if vertice == aresta.get_vertice_u:
+                adjs.append(aresta)
+            elif vertice == aresta.get_vertice_v:
+                adjs.append(aresta)
+            '''
             for vertices in self.vertices:
                 if(vertice == aresta.get_vertice_v and vertices == aresta.get_vertice_u):
-                    adjs.append(vertices)
+                    adjs.append(aresta)
                 elif(vertices == aresta.get_vertice_v and vertice == aresta.get_vertice_u):
-                    adjs.append(vertices)
+                    adjs.append(aresta)
+                    '''
         return adjs
-    
-    def caminho_dfs(self, vertice: Vertice, visitados: list[Vertice]):
-        visitados.append(vertice)
-        for adjacencia in self.adj(vertice):
-            if adjacencia not in visitados:
-                self.caminho_dfs(self, adjacencia, visitados)
-               
+
+    def caminho_dfs(self, visitados: list[Vertice]):
+        for vertice in self.vertices:
+            visitados.append(vertice)
+            for adjacencia in self.adj(vertice):
+                if adjacencia not in visitados:
+                    
+                    self.caminho_dfs(self, adjacencia, visitados)
+
+        
 class RedeResidual(Rede):
     
+    def set_fluxo_rede(self, v: Vertice):
+        fluxo_residual = self.adj(v)
+        return fluxo_residual
+        #for adjs in self.adj(v):
+            
+
+'''
     def definir_arestas_diretas(self):
         for aresta in self.arestas:
             if aresta.get_capacidade - aresta.get_fluxo > 0:
@@ -87,7 +107,16 @@ class RedeResidual(Rede):
                 aresta.set_vertice_u = verticev
                 aresta.set_vertice_v = verticeu
                 aresta.set_capacidade(aresta.get_fluxo)
-
+                    
+                
+    def caminho_dfs(self, visitados: list[Vertice]):
+        for vertice in self.vertices:
+            visitados.append(vertice)
+            for adjacencia in self.adj(vertice):
+                if adjacencia not in visitados:
+                    self.set_fluxo()
+                    self.caminho_dfs(self, adjacencia, visitados)
+'''                
 vertice1 = Vertice("Vertice1")
 vertice2 = Vertice("Vertice2")
 vertice3 = Vertice("Vertice3")
@@ -96,3 +125,9 @@ vertice4 = Vertice("Vertice4")
 aresta1 = Aresta(2,3,vertice1,vertice2)
 aresta2 = Aresta(3,5,vertice2,vertice3)
 aresta3 = Aresta(1,2,vertice2,vertice3)
+
+rede = Rede([vertice1,vertice2,vertice3,vertice4],[aresta1,aresta2,aresta3])
+rede_residual = RedeResidual([vertice1,vertice2,vertice3,vertice4],[aresta1,aresta2,aresta3])    
+
+print(rede.get_arestas())
+print(rede.adj(vertice1))
